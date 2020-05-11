5db307a975502a6275a29260e8f52d4b272a0c1f
Adri Forczek <forczekadri@gmail.com>
2020-05-11 23:17:23 +0100

    chore: adds GET and POST route

--- server.js
+++ server.js
@@ -7,8 +7,11 @@ const express = require('express');
 // Start up an instance of app
 const app = express();
 
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+// Dependencies
+const bodyParser = require('body-parser');
+
+// Middleware
+// Configuring express to use body-parser as middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -25,5 +28,23 @@ const server = app.listen(port, listening);
 
 // Callback function
 function listening(){
-  console.log(`server running on localhost: ${port}`);
+    console.log(`server running on localhost: ${port}`);
 }
+
+// HTTP REQUESTS
+
+// Get
+app.get('/', function (req, res) {
+    res.send(projectData);
+});
+
+// Post
+app.post('/addAnswer', addAnswer);
+function addAnswer (req, res) {
+    let data = req.body
+    // manually set the string for the key of the new JS object entry
+    projectData['temperature'] = data.temperature;
+    projectData['date'] = data.date;
+    projectData['user_response'] = data.user_response;
+    console.log(projectData);
+};
